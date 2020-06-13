965105ebd29eed8e98765c53cd67264c5be4fe7d
Lukaribou <lucasperroud17@gmail.com>
2020-06-13 20:25:14 +0200

    Update core.clj

--- src/police_rp/core.clj
+++ src/police_rp/core.clj
@@ -1,7 +1,42 @@
 (ns police-rp.core
-  (:gen-class))
+  (:gen-class)
+  (:require
+   [discljord.connections :as c]
+   [discljord.messaging :as m]
+   [discljord.events :as e]
+   [clojure.core.async :as a]
+   [clojure.string]
+   [police-rp.utils :as utils]))
+
+(def config (utils/read-config))
+(def state (atom nil))
+
+(defmulti handle-event
+  (fn [event-type _]
+    event-type))
+
+(defmethod handle-event :default
+  [_ _])
+
+(defmethod handle-event :ready
+  [_ _]
+  (c/status-update! (:connection @state) :activity (c/create-activity :name "the 911" :type :watch))) ;; Change l'activité du "Joue à"
+
+(defmethod handle-event :message-create
+  [_ {{bot :bot} :author :keys [channel-id content]}]
+  (when-not bot
+    (let [args (clojure.string/split content #" ")]
+      (m/create-message! (:messaging @state) channel-id :content (str "```Commande test\nArguments:```" args)))))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& _]
-  (println "Hello, World!"))
+  (let [event-ch (a/chan 100)
+        connection-ch (c/connect-bot! (:token config) event-ch)
+        messaging-ch (m/start-connection! (:token config))
+        init-state {:connection connection-ch
+                    :event event-ch
+                    :messaging messaging-ch}]
+    (reset! state init-state)
+    (e/message-pump! event-ch handle-event)
+    (m/stop-connection! messaging-ch)
+    (c/disconnect-bot! connection-ch)))
