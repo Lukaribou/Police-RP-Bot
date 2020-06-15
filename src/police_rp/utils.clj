c96b8f2d77661f0b83cb8724b7dcccc326c218b6
Lukaribou <lucasperroud17@gmail.com>
2020-06-15 23:23:37 +0200

    Update utils.clj

--- src/police_rp/utils.clj
+++ src/police_rp/utils.clj
@@ -3,7 +3,7 @@
   (:require
    [clojure.java.io :as io]
    [clojure.string :as str]
-   [cheshire.core]))
+   [cheshire.core :as cs]))
 
 
 (defn string-keys-to-symbols [map] ; https://markhneedham.com/blog/2013/09/26/clojure-writing-json-to-a-filereading-json-from-a-file/
@@ -14,5 +14,12 @@
       io/resource
       slurp
       str/trim
-      cheshire.core/parse-string
+      cs/parse-string
       string-keys-to-symbols))
+
+(defn db-save [name data]
+  ; https://dzone.com/articles/clojure-writing-json
+  (spit (io/resource (str "../resources/" name ".json")) (cs/generate-string data {:pretty true})))
+
+(defn db-get [name]
+  (cs/parse-string (slurp (io/resource (str "../resources/" name ".json")))))
