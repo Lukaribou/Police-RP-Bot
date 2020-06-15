b2e8742ad2a16448c19a365ba39cabe8451eeac8
Lukaribou <lucasperroud17@gmail.com>
2020-06-15 22:17:24 +0200

    Update cmd_handler.clj

--- src/police_rp/cmd_handler.clj
+++ src/police_rp/cmd_handler.clj
@@ -1,20 +1,19 @@
 (ns police-rp.cmd_handler
   (:gen-class)
   (:require
-   [discljord.messaging]))
+   [discljord.messaging]
+   [police-rp.commands.test]
+   [police-rp.commands.rp_init]))
+
+(def commands
+  {"test" #'police-rp.commands.test/execute
+   "rp-init" #'police-rp.commands.rp_init/execute})
 
 (defn redirect [cmd state & [options]]
-  (try
-    (require (symbol (str "police-rp.commands." cmd)))
-    (eval (str "(police-rp.commands." cmd "/execute " state " " options ")"))
-    (catch java.io.FileNotFoundException _
-      (discljord.messaging/send-message!
-       (:messaging @state)
-       (:channel-id options)
-       :content (str "Commande`" cmd "`non trouvée")))
-    (catch Exception e
-      (println (.getMessage e)))))
-      ;(discljord.messaging/send-message!
-       ;(:messaging @state)
-       ;(:channel-id options)
-       ;:content (str "Une erreur est survenue: " (.getMessage e))))))
+  (if (nil? (commands cmd))
+    (discljord.messaging/create-message! 
+     (:messaging @state) 
+     (:channel-id options) 
+     :content (str " Commande `" cmd "` introuvable"))
+    ((commands cmd) state options)))
+
